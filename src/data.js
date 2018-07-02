f25fe7d367918cbd2c3176a2bfce4d3a80a5f2f0
DianaOlvera <DianaOlvera@users.noreply.github.com>
2018-07-02 12:21:57 -0500

    barra de logo y usuario

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const url = `https://raw.githubusercontent.com/DianaOlvera/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`;
+fetch(url)
+.then(answer => answer.json())
+.then (answer => {
+
+
+console.log(answer)
+ })
+
+
+ window.computeStudentsStats = () => {
+
+ };
+ window.computeGenerationsStats = () => {
+
+ };
+ window.sortStudents = () => {
+
+ };
+ window.filterStudents = () => {
+   
+ };
