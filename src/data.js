829960f8f9d80c3e501a2028070db47eb367aa89
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-07-03 04:58:07 -0500

    Se pintan generaciones por sede

--- src/data.js
+++ src/data.js
@@ -1,26 +1,133 @@
 
 //Guardamos la ubicación del JSON en una variable
-//const json = "https://raw.githubusercontent.com/RaquelGlez/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
-
-const json = '../data/laboratoria.json';
+const json = "https://raw.githubusercontent.com/RaquelGlez/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
+// o jalarlo mediante el servidor
+//const json = '../data/laboratoria.json';
 
 const getData = () => {
   fetch(json)
   .then(response => response.json())
   .then((res) => {
     //console.log(res)
-    drawCampus(res);
+    drawCampus(res);  //activar para crear botones con sedes
+    //drawGen(res);
+    //window.computeGenerationsStats(res);  // linea necesaria para ejecutar función y poder ver en consola
 
   })
   .catch((error) =>{
     console.log('Error');
   });
 }
-
 getData();
 
-const drawCampus = (data) => {
-  const sedes = Object.keys(data);
+//
+// //Creando botones para visualizar las sedes dentro de  con botones de opciones de sedes, ¿como separar los botones?
+
+const drawCampus = (laboratoria) => {
+
+  const sedes = Object.keys(laboratoria);
+  console.log(sedes);
+  const verSedes = document.getElementById('ver-sedes');
+  sedes.forEach((sede) =>{
+    const option = document.createElement('button');
+    option.innerHTML = sede;
+    //option.addEventListener("click", gen)
+    verSedes.appendChild(option);
+    option.addEventListener("click", () => drawGen(laboratoria))
+    //option.addEventListener("click", () => gen(laboratoria))
+  });
+};
+
+const drawGen = (laboratoria) => {
+  //console.log(laboratoria[event.target.innerHTML].generacion);
+  //console.log(event.target.innerHTML);
+  //if(event.target.innerHTML == )
+
+
+
+  for( i in laboratoria[event.target.innerHTML].generacion){
+    //console.log(i);
+    let generaciones = i;
+    
+
+    const viewGen = document.getElementById('ver-gen');
+    const option = document.createElement('button');
+    option.innerHTML = generaciones;
+    viewGen.appendChild(option);
+    // console.log(generaciones);
+
+  };
+};
+
+
+
+
+
+//Ahora trabajamos con la funcion global, para obtener el objeto solicitado
+/*
+ window.computeGenerationsStats = (laboratoria) => {
+    const generationsArray = [];
+    // const obj = {
+    //     campus: '',
+    //     generation: '',
+    //     average: '',
+    //     count: ''
+    // };
+
+
+     for (key in laboratoria) {
+
+       //console.log(key);  //muestra las sedes
+       //console.log(laboratoria[key]);  // muestra las generaciones
+       //console.log(laboratoria[key].generacion);  // muestra un objeto con las generaciones por sede
+       //console.log(Object.keys(laboratoria[key].generacion));  //muestra un array con el nombre de las generaciones por sede
+
+       //guardar las keys en una variable
+       let generations = Object.keys(laboratoria[key].generacion);
+       generations.forEach((generation) => {
+         const obj = {
+           campus: key,
+           generation: generation,
+           count: laboratoria[key].generacion[generation].estudiantes.length
+         }
+//console.log(laboratoria[key].generacion[generation].estudiantes.length);
+         //console.log(key);
+
+            generationsArray.push(obj);
+
+          });
+
+//console.log(laboratoria[key].generacion);
+     }
+     console.log(generationsArray);
+     return generationsArray;
+  };*/
+//
+//       for (key in laboratoria){
+//         obj.campus = key;
+//         console.log(key);
+//          let generations = Object.keys(laboratoria[key].generacion);
+//        //  console.log(Object.keys(laboratoria[key].generacion));
+//          obj.generation = generation;
+//        //      console.log(generations);
+//          generations.forEach((generation) => {
+//            obj.generation = generation;
+//        //    console.log(generation);
+//             generationsArray.push(obj);
+//            console.log(generationsArray);
+//        // })
+//   };
+// };
+
+
+ //return generationsArray;
+//};
+
+
+
+//visualizar sedes mediante un select
+/*const drawCampus = (laboratoria) => {
+  const sedes = Object.keys(laboratoria);
   console.log(sedes);
   const containerCampus = document.getElementById('campus');
   sedes.forEach((sede) =>{
@@ -29,12 +136,7 @@ const drawCampus = (data) => {
     containerCampus.appendChild(option);
   });
   //containerCampus.addEventListener('change',iteratorGenerations);
-};
-
-
-
-
-
+};*/
 
 
 
@@ -68,39 +170,5 @@ window.sortStudents = () => {
 window.filterStudents = () => {
 
 };
-
-*/
-
-
-
-/*fetch(url).then(data => data.json())
-.then(data => {
-  console.log(data);
-});
-}
-obtainData();*/
-
-
-
-
-
-
-/*
-//Declaramos una variable que imprimira a las generaciones una vez que las saquemos del objeto json
-let generation = document.getElementById("ver-sedes");
-
-//Declaramos el evento que nos ayudara a imprimir las generaciones al dar click a un boton
-btngen.addEventListener("click",function () {
-  obtenerSed();
-})
-
-
-let obtenerSed = function() {
-    fetch(url).then((response)=>{
-      return response.json();
-    }).then((data) => {
-
-    })
-  }
 
 */
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 
-/*Aquí va todo lo referente a DOM
+/*Aquí va todo lo referente a DOM*/
 
-*/
+//Para registrar el login del usuario y acceder a la siguiente pantalla
+
+//let usuario = document.getElementById("usuario");
+//let password = document.getElementById("password");
+
+function enviarLogin() {
+  let usuario = document.getElementById("usuario");
+  let password = document.getElementById("password");
+  if ((usuario.value == "")||(password.value =="")) {
+    document.getElementById("visualizar-sedes").innerHTML = "Bienvenid@ " + usuario.toUpperCase();
+    login.style.display = "none";
+    visualizar.style.display = "block";
+  }else {
+    window.alert("Los campos usuario y contraseña no pueden estar vacios")
+  };
+};
