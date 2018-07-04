ea1b7e2c7904d181215dcb61150be55fadff8669
DianaOlvera <DianaOlvera@users.noreply.github.com>
2018-07-04 15:08:29 -0500

    actualizacion de login , muestra sedes y generaciones

--- src/data.js
+++ src/data.js
@@ -1,32 +1,8 @@
-<<<<<<< HEAD
-const url = `https://raw.githubusercontent.com/DianaOlvera/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`;
-fetch(url)
-.then(answer => answer.json())
-.then (answer => {
-
-
-console.log(answer)
- })
-
-
- window.computeStudentsStats = () => {
-
- };
- window.computeGenerationsStats = () => {
-
- };
- window.sortStudents = () => {
-
- };
- window.filterStudents = () => {
-   
- };
-=======
 
 //Guardamos la ubicación del JSON en una variable
-//const json = "https://raw.githubusercontent.com/RaquelGlez/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
+const json = "https://raw.githubusercontent.com/RaquelGlez/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
 
-const json = '../data/laboratoria.json';
+
 
 const getData = () => {
   fetch(json)
@@ -34,98 +10,32 @@ const getData = () => {
   .then((res) => {
     //console.log(res)
     drawCampus(res);
-
   })
   .catch((error) =>{
     console.log('Error');
   });
 }
-
 getData();
 
-const drawCampus = (data) => {
-  const sedes = Object.keys(data);
-  console.log(sedes);
-  const containerCampus = document.getElementById('campus');
+
+const drawCampus = (laboratoria) => {
+  const sedes = Object.keys(laboratoria);
+  const verSedes = document.getElementById("versedes");
   sedes.forEach((sede) =>{
-    const option = document.createElement('option');
+    const option = document.createElement("button");
+    //const t = document.createTextNode('sede')
     option.innerHTML = sede;
-    containerCampus.appendChild(option);
+    verSedes.appendChild(option);
+      option.addEventListener("click", () => drawGeneraciones(laboratoria))
   });
-  //containerCampus.addEventListener('change',iteratorGenerations);
 };
+const drawGeneraciones =(laboratoria)=> {
+  for(i in laboratoria[event.target.innerHTML].generacion){
+    let generaciones =i;
 
-
-
-
-
-
-
-
-
-/*
-
-window.computeGenerationsStats = (laboratoria) => {
-  const generationsArray = [];
-  const obj = {
-    campus: '',
-    generation: '',
-    average: 0,
-    count: 0,
+    const viewGen = document.getElementById("ver-gen");
+    const option = document.createElement("button");
+    option.innerHTML = generaciones;
+    viewGen.appendChild(option);
   };
-  let average = 0;
-  for (key in laboratoria){
-    obj
-  }
 };
-
-window.computeStudentsStats = () => {
-
-};
-
-
-window.sortStudents = () => {
-
-};
-
-
-window.filterStudents = () => {
-
-};
-
-*/
-
-
-
-/*fetch(url).then(data => data.json())
-.then(data => {
-  console.log(data);
-});
-}
-obtainData();*/
-
-
-
-
-
-
-/*
-//Declaramos una variable que imprimira a las generaciones una vez que las saquemos del objeto json
-let generation = document.getElementById("ver-sedes");
-
-//Declaramos el evento que nos ayudara a imprimir las generaciones al dar click a un boton
-btngen.addEventListener("click",function () {
-  obtenerSed();
-})
-
-
-let obtenerSed = function() {
-    fetch(url).then((response)=>{
-      return response.json();
-    }).then((data) => {
-
-    })
-  }
-
-*/
->>>>>>> upstream/master
--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 
-/*Aquí va todo lo referente a DOM
+//Aquí va todo lo referente a DOM
 
-*/
+//Para registrar el login del usuario y acceder a la siguiente pantalla
+  let us = document.getElementById("user");
+  let password1 = document.getElementById("password");
+  let ingresar = document.getElementById("btn-login");
+  let encabezados =document.getElementById("encabezado");
+
+  let visualizar =document.getElementById("visualizar-sedes");
+  visualizar.style.display = "none";
+  encabezados.style.display="none";
+
+
+// Botones de login
+ingresar.addEventListener("click", datosUsuario = ()=> {
+    let usuario = us.value;
+    let password2 = password1.value;
+        if ((usuario == "Raque")||(password2 =="R2")) {
+          login.style.display="none";
+          logoPrincipal.style.display="none";
+          visualizar.style.display = "block";
+          encabezados.style.display = "block";
+    }else{
+        alert("Datos Incorrectos");
+}
+});
+
+// para segunda pantalla
